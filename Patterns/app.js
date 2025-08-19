128dd9ba4cae57ba0eca354a595b77b4b54200b2
charan-gurram <sricharangurram23@gmail.com>
2025-08-19 21:07:05 +0530

    Patterns

---
+++ Patterns/app.js
@@ -0,0 +1,57 @@
+// const str= process.argv[2];
+// console.log(str)
+// for(let i=0;i<str.length;i++)
+// {
+//     for(let j=0;j<=i;j++)
+//     {
+//         process.stdout.write(str[j])
+        
+//     }
+//     console.log()
+// }
+
+
+// Right-angle stars pattern
+
+// const num =process.argv[2];
+// for(let i=0;i<num;i++)
+// {
+//     let str=" ";
+//     for(let j=0;j<=i;j++)
+//     {
+//          str+="*";    
+//     }
+//     console.log(str)
+// }
+
+
+
+// Reverse Right angled pattern
+
+// const num =process.argv[2];
+// for(let i=num;i>0;i--)
+// {
+//     let str=" ";
+//     for(let j=0;j<i;j++)
+//     {
+//          str+="*";    
+//     }
+//     console.log(str)
+// }
+
+// Square  pattern
+
+const num =process.argv[2];
+for(let i=0;i<num;i++)
+{
+    let str=" ";
+    for(let j=0;j<=num;j++)
+    {
+         str+="*";    
+    }
+    console.log(str)
+}
+
+
+// even and odd pattern
+
