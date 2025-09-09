bd41f326797d313307175606fe69059a17529189
charan-gurram <sricharangurram23@gmail.com>
2025-09-09 22:25:55 +0530

    MouseEvents

---
+++ Events/Examples/MouseEvents/BgColor/app.js
@@ -0,0 +1,7 @@
+const btn=document.getElementById("btn");
+btn.addEventListener("mouseenter",()=>{
+    btn.style.backgroundColor="yellow";
+})
+btn.addEventListener("mouseleave",()=>{
+    btn.style.backgroundColor="";
+})
